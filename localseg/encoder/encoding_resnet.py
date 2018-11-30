255e7cdf4dede9f33f23cf94daafee6f4148f4f7
Marvin Teichmann <marvin.teichmann@googlemail.com>
2018-11-30 18:08:55 +0000

    Implement group norm in encoding.

--- localseg/encoder/encoding_resnet.py
+++ localseg/encoder/encoding_resnet.py
@@ -33,15 +33,17 @@ def conv3x3(in_planes, out_planes, stride=1):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, dilation=1, downsample=None, first_dilation=1): # NOQA
+    def __init__(self, inplanes, planes, stride=1, dilation=1,
+                 downsample=None, first_dilation=1, bn=None): # NOQA
         super(BasicBlock, self).__init__()
+        self.BatchNorm = bn
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
                                padding=dilation, dilation=dilation, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = self.BatchNorm(planes)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
                                padding=first_dilation, dilation=first_dilation, bias=False) # NOQA
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = self.BatchNorm(planes)
         self.downsample = downsample
         self.stride = stride
 
@@ -68,16 +70,18 @@ class Bottleneck(nn.Module):
     expansion = 4
 
     def __init__(self, inplanes, planes, stride=1, dilation=1,
-                 downsample=None, first_dilation=1, use_s2b=False):
+                 downsample=None, first_dilation=1, use_s2b=False,
+                 bn=None):
+        self.BatchNorm = bn
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = self.BatchNorm(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
                                padding=dilation, dilation=dilation, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = self.BatchNorm(planes)
         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1,
                                bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.bn3 = self.BatchNorm(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.dilation = dilation
@@ -136,12 +140,13 @@ class ResNet(nn.Module):
     """
 
     def __init__(self, block, layers, num_classes=1000, dilated=True,
-                 batched_dilation=False):
+                 batched_dilation=False, bn=None):
         self.inplanes = 64
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.BatchNorm = bn
+        self.bn1 = self.BatchNorm(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
@@ -188,7 +193,7 @@ class ResNet(nn.Module):
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, self.BatchNorm):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
@@ -198,13 +203,11 @@ class ResNet(nn.Module):
     def get_channel_dict(self):
 
         bn4 = [m for m in self.layer4.modules()][-2]
-        assert(isinstance(bn4, nn.BatchNorm2d))
+        assert(isinstance(bn4, self.BatchNorm))
         bn3 = [m for m in self.layer3.modules()][-2]
-        assert(isinstance(bn3, nn.BatchNorm2d))
+        assert(isinstance(bn3, self.BatchNorm))
         bn2 = [m for m in self.layer2.modules()][-2]
-        assert(isinstance(bn2, nn.BatchNorm2d))
-
-        nn.BatchNorm2d
+        assert(isinstance(bn2, self.BatchNorm))
 
         if self.dilated:
             df = 8
@@ -226,26 +229,29 @@ class ResNet(nn.Module):
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
+                self.BatchNorm(planes * block.expansion),
             )
 
         layers = []
         if dilation == 1 or dilation == 2:
             layers.append(block(self.inplanes, planes, stride, dilation=1,
                                 downsample=downsample,
-                                first_dilation=dilation, use_s2b=use_s2b))
+                                first_dilation=dilation, use_s2b=use_s2b,
+                                bn=self.BatchNorm))
         elif dilation == 4:
             assert not use_s2b
             layers.append(block(self.inplanes, planes, stride, dilation=2,
                                 downsample=downsample,
-                                first_dilation=dilation))
+                                first_dilation=dilation,
+                                bn=self.BatchNorm))
         else:
             raise RuntimeError("=> unknown dilation size: {}".format(dilation))
 
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes,
-                                dilation=dilation, first_dilation=dilation))
+                                dilation=dilation, first_dilation=dilation,
+                                bn=self.BatchNorm))
 
         return nn.Sequential(*layers)
 
