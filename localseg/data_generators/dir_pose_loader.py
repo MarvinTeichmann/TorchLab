Fix rotation loss.

localseg/data_generators/dir_pose_loader.py
@@ -274,15 +274,14 @@ class DirLoader(data.Dataset):
                       label_dict["geo_world"].astype(np.float32)}
             return sample
 
-        assert meta_dict['updated']
-
-        assert np.abs(np.linalg.norm(meta_dict['Q_posenet']) - 1) < 0.00001, \
-            "Rotation not normalized: {}".format(
-                np.abs(np.linalg.norm(meta_dict['Q_posenet'])))
+        if meta_dict['Q_posenet'][0] < 0:
+            rotation = -1 * meta_dict['Q_posenet']
+        else:
+            rotation = meta_dict['Q_posenet']
 
         sample = {
             'image': image,
-            'rotation': meta_dict['Q_posenet'],
+            'rotation': rotation,
             'rotation_gt': meta_dict['R_opensfm'],
             'translation': meta_dict['T_posenet'],
             'translation_gt': meta_dict['T_opensfm'],
@@ -670,6 +669,10 @@ class DirLoader(data.Dataset):
                         label_dict[key] = new_item
 
                     image = new_img
+        else:
+            patch_size = transform['patch_size']
+            image, label_dict = center_crop(
+                image, label_dict, patch_size, load_dict)
 
         for key, item in label_dict.items():
             assert image.shape[:2] == item.shape[:2], \
@@ -697,6 +700,8 @@ class DirLoader(data.Dataset):
             center_w, width, quaternion, load_dict['rolled'])
 
         meta_dict['Q_posenet'] = quaternion_new
+
+        # print(quaternion_new, quaternion)
 
         meta_dict['updated'] = True
 

localseg/evaluators/poseevaluator.py
@@ -73,15 +73,16 @@ class MetaEvaluator(object):
 
         val_iter = self.conf['evaluation']["val_subsample"]
         train_iter = self.conf['evaluation']["train_subsample"]
-        # tdo_agumentation = self.conf['evaluation']["train_do_agumentation"]
+        tdo_agumentation = self.conf['evaluation']["train_do_agumentation"]
+        do_agumentation = self.conf['evaluation']["val_do_agumentation"]
 
         self.val_evaluator = Evaluator(
             conf, model, val_iter, name="Val", split="val",
-            imgdir=self.imgdir, do_augmentation=True)
+            imgdir=self.imgdir, do_augmentation=do_agumentation)
         self.train_evaluator = Evaluator(
             conf, model, train_iter, name="Train",
             split='train', imgdir=self.imgdir,
-            do_augmentation=True)
+            do_augmentation=tdo_agumentation)
 
         self.evaluators = []
 
@@ -202,7 +203,7 @@ class Evaluator():
             batch_size = 8
 
         if conf['evaluation']['reduce_val_bs']:
-            batch_size = 4 * torch.cuda.device_count()
+            batch_size = torch.cuda.device_count()
 
         subsampler = partial(
             sampler.SubSampler, subsample=subsample)

localseg/utils/quaternion.py
@@ -36,23 +36,26 @@ def min_angle(q0, q1):
     assert len(q0.shape) in [1, 2]
     assert len(q1.shape) in [1, 2]
 
-    q0 = normalize(q0).double()
-    q1 = normalize(q1).double()
+    q0 = normalize(q0)
+    q1 = normalize(q1)
 
     q0q1 = multiply(q0, conjugate(q1))
 
     nq0q1 = normalize(q0q1)
 
     if len(nq0q1.shape) == 1:
-        return 2 * torch.acos(torch.abs(nq0q1[0]))
+        return - nq0q1[0].pow(2)
+        # return 2 * torch.acos(torch.abs(nq0q1[0]))
     else:
-        return 2 * torch.acos(torch.abs(nq0q1[:, 0]))
+        result = nq0q1[:, 0]
+        return - (result * result)
+        # return 2 * torch.acos(torch.abs(nq0q1[:, 0]))
 
 
 def conjugate(q):
 
     if len(q.shape) == 1:
-        return torch.Tensor([q[0], -q[1], -q[2], -q[3]]).double()
+        return torch.Tensor([q[0], -q[1], -q[2], -q[3]])
     elif len(q.shape) == 2:
         result = [q[:, 0], -q[:, 1], -q[:, 2], -q[:, 3]]
         return torch.stack(result, dim=1)
@@ -69,7 +72,7 @@ def multiply(q0, q1):
             [-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
              x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
              -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
-             x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0]).double()
+             x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0])
 
         return result
     elif len(q0.shape) == 2: