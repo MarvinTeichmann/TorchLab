f390f4d365903077756dcc2203b92331143dd844
Marvin Teichmann <marvin.teichmann@googlemail.com>
2023-08-24 04:51:25 +0200

    Minor modifications on trainer.

--- torchlab/trainer2.py
+++ torchlab/trainer2.py
@@ -24,35 +24,40 @@ try:
 except ImportError:
     pass
 
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
-                    level=logging.INFO,
-                    stream=sys.stdout)
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    level=logging.INFO,
+    stream=sys.stdout,
+)
 
 
-class Trainer():
-
+class Trainer:
     def __init__(self, conf, model, data_loader, logger=None):
         self.model = model
         self.conf = conf
 
-        self.bs = conf['training']['batch_size']
-        self.lr = conf['training']['learning_rate']
-        self.wd = conf['training']['weight_decay']
-        self.clip_norm = conf['training']['clip_norm']
+        self.bs = conf["training"]["batch_size"]
+        self.lr = conf["training"]["learning_rate"]
+        self.wd = conf["training"]["weight_decay"]
+        self.clip_norm = conf["training"]["clip_norm"]
         # TODO: implement clip norm
 
-        self.max_epochs = conf['training']['max_epochs']
-        self.eval_iter = conf['logging']['eval_iter']
-        self.mayor_eval = conf['logging']['mayor_eval']
-        self.checkpoint_backup = conf['logging']['checkpoint_backup']
-        self.max_epoch_steps = conf['training']['max_epoch_steps']
+        self.max_epochs = conf["training"]["max_epochs"]
+        self.eval_iter = conf["logging"]["eval_iter"]
+        self.mayor_eval = conf["logging"]["mayor_eval"]
+        self.checkpoint_backup = conf["logging"]["checkpoint_backup"]
+        self.max_epoch_steps = conf["training"]["max_epoch_steps"]
 
         mulsampler = partial(
-            sampler.RandomMultiEpochSampler, multiplicator=self.eval_iter)
+            sampler.RandomMultiEpochSampler, multiplicator=self.eval_iter
+        )
 
         self.loader = data_loader.get_data_loader(
-            conf['dataset'], split='train', batch_size=self.bs,
-            sampler=mulsampler)
+            conf["dataset"],
+            split="train",
+            batch_size=self.bs,
+            sampler=mulsampler,
+        )
 
         # mysampler = sampler.RandomMultiEpochSampler(dataset, self.eval_iter)
         # mysampler = RandomSampler(dataset)
@@ -66,7 +71,6 @@ class Trainer():
         start_time = time.time()
 
         for step, sample in enumerate(self.loader):
-
             if step == 100:
                 break
 
@@ -78,7 +82,6 @@ class Trainer():
         start_time = time.time()
 
         for step, sample in enumerate(self.loader):
-
             if step == 100:
                 break
 
@@ -88,64 +91,80 @@ class Trainer():
         logging.info("Loading 100 examples took: {}".format(duration))
 
     def init_trainer(self):
-
         self.initialized = True
 
         self.logger = self.model.logger
 
-        self.device = self.conf['training']['device']
+        self.device = self.conf["training"]["device"]
         self.model.device = self.device
 
-        self.checkpoint_name = os.path.join(self.model.logdir,
-                                            'checkpoint.pth.tar')
+        self.checkpoint_name = os.path.join(
+            self.model.logdir, "checkpoint.pth.tar"
+        )
 
-        backup_dir = os.path.join(self.model.logdir, 'backup')
+        backup_dir = os.path.join(self.model.logdir, "backup")
         if not os.path.exists(backup_dir):
             os.mkdir(backup_dir)
 
-        self.log_file = os.path.join(self.model.logdir, 'summary.log.hdf5')
+        self.log_file = os.path.join(self.model.logdir, "summary.log.hdf5")
 
         weight_dicts = self.model.get_weight_dicts()
 
-        if self.conf['training']['optimizer'] == 'adam':
-
+        if self.conf["training"]["optimizer"] == "adam":
             self.optimizer = torch.optim.Adam(weight_dicts, lr=self.lr)
 
-        elif self.conf['training']['optimizer'] == 'SGD':
-            momentum = self.conf['training']['momentum']
-            self.optimizer = torch.optim.SGD(weight_dicts, lr=self.lr,
-                                             momentum=momentum)
+        elif self.conf["training"]["optimizer"] == "SGD":
+            momentum = self.conf["training"]["momentum"]
+            self.optimizer = torch.optim.SGD(
+                weight_dicts, lr=self.lr, momentum=momentum
+            )
 
         else:
             raise NotImplementedError
 
     def update_lr(self):
+        conf = self.conf["training"]
+        lr_schedule = conf["lr_schedule"]
 
-        conf = self.conf['training']
-        lr_schedule = conf['lr_schedule']
+        self.step = self.step + 1
+
+        step = self.step
+        mstep = self.max_steps
+
+        warm_up_steps = conf["warm_up_epochs"] * self.epoch_steps
+
+        if step < warm_up_steps:
+            base_lr = conf["learning_rate"]
+
+            lr = base_lr / warm_up_steps * step
+
+            _set_lr(self.optimizer, lr)
+            return lr
+        else:
+            step -= warm_up_steps
+            mstep -= warm_up_steps
 
         if lr_schedule == "constant":
-            self.step = self.step + 1
             return
-            pass
+
         elif lr_schedule == "poly":
-            self.step = self.step + 1
-            base = conf['base']
-            min_lr = conf['min_lr']
-            base_lr = conf['learning_rate']
-            step = self.step
-            mstep = self.max_steps
+            base = conf["base"]
+            min_lr = conf["min_lr"]
+            base_lr = conf["learning_rate"]
 
             assert step <= mstep
             assert min_lr < base_lr
             base_lr -= min_lr
 
-            lr = base_lr * ((1 - step / mstep)**base)**conf['base2'] + min_lr
+            lr = (
+                base_lr * ((1 - step / mstep) ** base) ** conf["base2"]
+                + min_lr
+            )
         elif lr_schedule == "exp":
             self.step = self.step
-            exp = conf['exp']
-            base_lr = conf['learning_rate']
-            min_lr = conf['min_lr']
+            exp = conf["exp"]
+            base_lr = conf["learning_rate"]
+            min_lr = conf["min_lr"]
             step = self.step
             mstep = self.max_steps
 
@@ -153,7 +172,7 @@ class Trainer():
             assert min_lr < base_lr
             base_lr -= min_lr
 
-            lr = base_lr * 10**(- exp * step / mstep)
+            lr = base_lr * 10 ** (-exp * step / mstep)
         else:
             raise NotImplementedError
 
@@ -162,10 +181,9 @@ class Trainer():
         return lr
 
     def get_lr(self):
-        return self.optimizer.param_groups[0]['lr']
+        return self.optimizer.param_groups[0]["lr"]
 
     def do_training_step(self, step, sample):
-
         # Do forward pass
         # img_var = sample['image'].to(self.device)  # TODO Remove?
 
@@ -179,19 +197,24 @@ class Trainer():
         self.optimizer.zero_grad()
         total_loss.backward()
 
-        clip_norm = self.conf['training']['clip_norm']
+        clip_norm = self.conf["training"]["clip_norm"]
         if clip_norm is not None:
             totalnorm = torch.nn.utils.clip_grad.clip_grad_norm(
-                self.model.parameters(), clip_norm)
+                self.model.parameters(), clip_norm
+            )
         else:
             totalnorm = 0
-            parameters = list(filter(
-                lambda p: p.grad is not None, self.model.network.parameters()))
+            parameters = list(
+                filter(
+                    lambda p: p.grad is not None,
+                    self.model.network.parameters(),
+                )
+            )
             for p in parameters:
                 norm_type = 2
                 param_norm = p.grad.data.norm(norm_type)
                 totalnorm += param_norm.item() ** norm_type
-            totalnorm = totalnorm ** (1. / norm_type)
+            totalnorm = totalnorm ** (1.0 / norm_type)
 
         self.optimizer.step()
 
@@ -203,25 +226,26 @@ class Trainer():
             self.losses.append(total_loss.item())
 
             epoch_string = "Epoch [{:5d}/{:5d}][{:4d}/{:4d}]  ".format(
-                self.epoch, self.max_epochs, step, self.epoch_steps)
+                self.epoch, self.max_epochs, step, self.epoch_steps
+            )
 
-            log_str1 = (" LR: {:.3E}"
-                        " Speed: {:.1f} imgs/sec ({:.3f} s/batch)"
-                        " GradNorm: {:6.2f}")
+            log_str1 = (
+                " LR: {:.3E}"
+                " Speed: {:.1f} imgs/sec ({:.3f} s/batch)"
+                " GradNorm: {:2.2f}"
+            )
 
             lr = self.get_lr()
 
-            for_str = log_str1.format(
-                lr, imgs_per_sec, duration, totalnorm)
+            for_str = log_str1.format(lr, imgs_per_sec, duration, totalnorm)
 
             " " * len(epoch_string)
 
             loss_names = [key for key in loss_dict.keys()]
             loss_vals = [value.item() for value in loss_dict.values()]
 
-            loss_str = (len(loss_names) * "{:}: {:5.2f}  ")
-            formatted = loss_str.format(
-                *it.chain(*zip(loss_names, loss_vals)))
+            loss_str = len(loss_names) * "{:}: {:2.2f} "
+            formatted = loss_str.format(*it.chain(*zip(loss_names, loss_vals)))
 
             logging.info(epoch_string + formatted + for_str)
 
@@ -230,7 +254,6 @@ class Trainer():
         return loss_dict
 
     def train(self, max_epochs=None):
-
         if not self.initialized:
             logging.error("Trainer not initialized.")
             logging.info("Run trainer.init_trainer() before starting traing.")
@@ -255,27 +278,28 @@ class Trainer():
 
         self.step = self.epoch_steps * math.ceil(self.epoch / self.eval_iter)
 
-        assert(self.step >= self.epoch)
+        assert self.step >= self.epoch
 
         self.display_iter = max(
-            1, self.epoch_steps // self.conf['logging']['disp_per_epoch'])
+            1, self.epoch_steps // self.conf["logging"]["disp_per_epoch"]
+        )
 
         if self.epoch > 0:
-            logging.info('Continue Training from {}'.format(self.epoch))
+            logging.info("Continue Training from {}".format(self.epoch))
         else:
             logging.info("Start Training")
 
-        if self.conf['training']['pre_eval']:
-            level = self.conf['evaluation']['default_level']
+        if self.conf["training"]["pre_eval"]:
+            level = self.conf["evaluation"]["default_level"]
             self.model.evaluate(level=level)
 
         for epoch in range(self.epoch, max_epochs, self.eval_iter):
-
             self.epoch = epoch
             self.model.epoch = epoch
 
             assert self.step == self.epoch_steps * math.ceil(
-                self.epoch / self.eval_iter)
+                self.epoch / self.eval_iter
+            )
 
             epoche_time = time.time()
             self.losses = []
@@ -290,71 +314,87 @@ class Trainer():
 
             # Epoche Finished
             duration = (time.time() - epoche_time) / 60
-            logging.info("Finished Epoch {} in {} minutes"
-                         .format(epoch, duration))
+            logging.info(
+                "Finished Epoch {} in {} minutes".format(epoch, duration)
+            )
 
             if not self.epoch % self.eval_iter or self.epoch == max_epochs:
+                level = self.conf["evaluation"]["default_level"]
+                if (
+                    self.epoch > 0
+                    and not self.epoch % self.mayor_eval
+                    or self.epoch == max_epochs
+                ):
+                    level = "mayor"
 
-                level = self.conf['evaluation']['default_level']
-                if self.epoch > 0 and not self.epoch % self.mayor_eval or \
-                        self.epoch == max_epochs:
-                    level = 'mayor'
-
-                if self.epoch == 0 and \
-                        self.conf['evaluation']['first_eval_is_mayor']:
-                    level = 'mayor'
+                if (
+                    self.epoch == 0
+                    and self.conf["evaluation"]["first_eval_is_mayor"]
+                ):
+                    level = "mayor"
 
                 self.logger.init_step(epoch + self.eval_iter)
-                self.logger.add_value(self.losses, 'loss',
-                                      epoch + self.eval_iter)
+                self.logger.add_value(
+                    self.losses, "loss", epoch + self.eval_iter
+                )
 
                 np_values = [val.item() / 100 for val in loss_dict.values()]
                 loss_dict_np = dict(zip(loss_dict.keys(), np_values))
 
-                self.logger.add_values(value_dict=loss_dict_np,
-                                       prefix='losses',
-                                       step=epoch + self.eval_iter)
+                self.logger.add_values(
+                    value_dict=loss_dict_np,
+                    prefix="losses",
+                    step=epoch + self.eval_iter,
+                )
 
                 self.model.evaluate(epoch + self.eval_iter, level=level)
-                if self.conf['logging']['log']:
-                    logging.info("Saving checkpoint to: {}".format(
-                        self.model.logdir))
+                if self.conf["logging"]["log"]:
+                    logging.info(
+                        "Saving checkpoint to: {}".format(self.model.logdir)
+                    )
                     # Save Checkpoint
                     self.logger.save(filename=self.log_file)
                     self.logger.save(filename=self.log_file + ".back")
                     state = {
-                        'epoch': epoch + self.eval_iter,
-                        'step': self.step,
-                        'conf': self.conf,
-                        'state_dict': self.model.network.state_dict(),
-                        'optimizer': self.optimizer.state_dict()}
+                        "epoch": epoch + self.eval_iter,
+                        "step": self.step,
+                        "conf": self.conf,
+                        "state_dict": self.model.network.state_dict(),
+                        "optimizer": self.optimizer.state_dict(),
+                    }
 
                     torch.save(state, self.checkpoint_name)
                     # torch.save(state, self.checkpoint_name + ".back")
                     logging.info("Checkpoint saved sucessfully.")
                 else:
-                    logging.info("Output can be found: {}".format(
-                        self.model.logdir))
+                    logging.info(
+                        "Output can be found: {}".format(self.model.logdir)
+                    )
 
             if not self.epoch % 20 * self.eval_iter:
                 new_file = os.path.join(
-                    self.model.logdir, 'backup',
-                    'summary.log.{}.pickle'.format(self.epoch))
+                    self.model.logdir,
+                    "backup",
+                    "summary.log.{}.pickle".format(self.epoch),
+                )
                 self.logger.save(filename=new_file)
 
-            if self.epoch > 0 and self.checkpoint_backup and \
-                    not self.epoch % self.checkpoint_backup:
-                name = 'checkpoint_{:04d}.pth.tar'.format(self.epoch)
-                checkpoint_name = os.path.join(
-                    self.model.logdir, name)
+            if (
+                self.epoch > 0
+                and self.checkpoint_backup
+                and not self.epoch % self.checkpoint_backup
+            ):
+                name = "checkpoint_{:04d}.pth.tar".format(self.epoch)
+                checkpoint_name = os.path.join(self.model.logdir, name)
 
                 self.logger.save(filename=self.log_file)
                 state = {
-                    'epoch': epoch + self.eval_iter,
-                    'step': self.step,
-                    'conf': self.conf,
-                    'state_dict': self.model.network.state_dict(),
-                    'optimizer': self.optimizer.state_dict()}
+                    "epoch": epoch + self.eval_iter,
+                    "step": self.step,
+                    "conf": self.conf,
+                    "state_dict": self.model.network.state_dict(),
+                    "optimizer": self.optimizer.state_dict(),
+                }
                 torch.save(state, checkpoint_name)
 
                 torch.save(state, self.checkpoint_name)
@@ -365,10 +405,9 @@ SegmentationTrainer = Trainer
 
 
 def _set_lr(optimizer, learning_rate):
-
     for param_group in optimizer.param_groups:
-        param_group['lr'] = learning_rate
+        param_group["lr"] = learning_rate
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.info("Hello World.")
